756ce4d2743fc39c742690d17f93d79eda4bfe25
Andreas Lindner <andili@kth.se>
2020-04-20 16:06:59 +0200

    Tune final output

--- scripts/1_validate_branch.sh
+++ scripts/1_validate_branch.sh
@@ -39,8 +39,11 @@ done
 "${HOLBA_DIR}/src/tools/scamv/examples/scripts/3-run.sh" arm8/exps2
 
 # 6. print information
+echo ""
+echo "=================================================================="
 echo "Experiment rerunning has finished, please terminate the connection to the board in the new terminal window and check the results manually now using \"git diff\""
-
+echo ""
+echo "=================================================================="
 echo "Number of experiment runs after rerunning: "
 find arm8 -name run.* | wc -l
 
